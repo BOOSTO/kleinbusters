61902782c8dd31ad689e2139dcbce887c8242832
Ethan <ethan@hirschfamily.com>
2022-09-20 14:21:13 -0700

    rnc

--- gamemode/round_controller/cl_round_controller.lua
+++ gamemode/round_controller/cl_round_controller.lua
@@ -1,3 +1,20 @@
+KB__IDLE = 0
+KB__START = 1
+KB__END = 2
 
+local round_status = KB__IDLE
+
+net.Receive("UpdateRoundStatus", function(len)
+
+    round_status = net.ReadInt(4)
+    print("-- round_status: "..round_status)
+
+end)
+
+function getRoundStatus()
+
+    return round_status
+
+end
 
 print("CLIENT: Round Controller")
--- gamemode/round_controller/sv_round_controller.lua
+++ gamemode/round_controller/sv_round_controller.lua
@@ -1,3 +1,35 @@
+KB__IDLE = 0
+KB__START = 1
+KB__END = 2
 
+local round_status = KB__IDLE
 
-print("SERVER: Round Controller")
+util.AddNetworkString("UpdateRoundStatus")
+
+function beginRound()
+
+    round_status = KB__START
+    updateClientRoundStatus()
+
+end
+
+function endRound()
+
+    round_status = KB__END
+    updateClientRoundStatus()
+
+end
+
+function getRoundStatus()
+
+    return round_status
+
+end
+
+function updateClientRoundStatus()
+
+    net.Start("UpdateRoundStatus")
+        net.WriteInt(round_status, 4)
+    net.Broadcast()
+
+end
