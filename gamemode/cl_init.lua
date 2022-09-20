d14e14efbe2c9419cd47edceb2473d4ce41f11ef
Ethan <ethan@hirschfamily.com>
2022-09-20 14:32:30 -0700

    add lobby code

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -3,3 +3,4 @@ include("shared.lua")
 -- Include other files
 
 include("round_controller/cl_round_controller.lua")
+include("lobby_manager/cl_lobby_manager.lua")
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,6 +6,8 @@ include("shared.lua")
 -- Include other scripts
 AddCSLuaFile("round_controller/cl_round_controller.lua")
 include("round_controller/sv_round_controller.lua")
+AddCSLuaFile("lobby_manager/cl_lobby_manager.lua")
+include("lobby_manager/sv_lobby_manager.lua")
 
 function GM:PlayerConnect(name, ip)
 
