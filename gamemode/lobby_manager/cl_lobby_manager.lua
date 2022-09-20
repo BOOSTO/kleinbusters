14e69f8f4b55ff5f89365ff9b4ad5fd50180901b
Ethan <ethan@hirschfamily.com>
2022-09-20 15:11:47 -0700

    basic lobby sghit

---
+++ gamemode/lobby_manager/cl_lobby_manager.lua
@@ -0,0 +1,26 @@
+function openLobby()
+    
+    local frame = vgui.Create("DFrame")
+    frame:SetSize(ScrW(), ScrH())
+    frame:Center()
+    frame:SetVisible(true)
+    -- frame:ShowCloseButton(false)
+    frame:SetDraggable(false)
+    frame.Paint = function(s, w, h)
+        draw.RoundedBox(0,0,0,w,h,Color(0,0,0,255))
+    end
+    frame:MakePopup()
+
+    local startBut = vgui.Create("DButton", frame)
+    startBut:SetSize(200, 75)
+    startBut:SetPos(ScrW() / 2 - 100, ScrH() / 2 - (75 / 2))
+    startBut:SetText("okay")
+    startBut.DoClick = function()
+        net.Start("ready_up")
+        net.SendToServer()
+        frame:Close()
+    end
+
+end
+
+net.Receive("open_lobby", openLobby)
--- gamemode/lobby_manager/sv_lobby_manager.lua
+++ gamemode/lobby_manager/sv_lobby_manager.lua
@@ -1,1 +1,17 @@
 util.AddNetworkString("open_lobby")
+util.AddNetworkString("ready_up")
+
+function enterLobby()
+
+    net.Start("open_lobby")
+    net.Broadcast()
+
+end
+
+net.Receive("ready_up", function()
+
+    beginRound()
+
+end)
+
+hook.Add("PlayerInitialSpawn", "open_player_lobby", enterLobby)
